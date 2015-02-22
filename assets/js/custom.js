8793c4f195d9bd9b35a160a225190e94c9493a81
Justin Cano <jcano001@ucr.edu>
2015-02-22 11:36:02 -0800

    added playback controls for carousel

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -8,6 +8,17 @@ window.onload = function() {
     owl.trigger('owl.play', 5000);
   });
 
+  $('#play').click(function() {
+    var owl = $(this.dataset.target);
+    owl.trigger('owl.next');
+    owl.trigger('owl.play',5000); //owl.play event accept autoPlay speed as second parameter
+  });
+
+  $('#pause').click(function() {
+    var owl = $(this.dataset.target);
+    owl.trigger('owl.stop');
+  });
+
   $('.video').each(function() {
     this.onplay = function() {
       owl.trigger('owl.stop');
