3874b45becece1ec2fb850d8a6eeb0df2bf652b5
Justin Cano <jcano001@ucr.edu>
2015-02-22 11:11:04 -0800

    added js methods to pause carousel on video play

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,9 +1,21 @@
 window.onload = function() {
+ var owl = $('.owl-carousel');
  var sprite = new Motio($('.sprite')[0], {
     fps: 20,
     frames: 31
   });
   sprite.toEnd(false, function() {
-    owl.play();
+    owl.trigger('owl.play', 5000);
+  });
+
+  $('.video').each(function() {
+    this.onplay = function() {
+      owl.trigger('owl.stop');
+      console.log('video playing!');
+    };
+    this.onended = function () {
+      owl.trigger('owl.play', 5000);
+      console.log('video ended!');
+    };
   });
 }
